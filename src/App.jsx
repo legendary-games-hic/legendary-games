b1e93a3cfc89161cdbe731ad63686ebbd9a0c70c
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-10-31 11:27:10 -0400

    route setup and bootstrap installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import './assets/css/App.css'
 import { Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<h1>main</h1>}/>
-      <Route path='/games' element={<h1>games</h1>}/>
-      <Route path='/about' element={<h1>about</h1>}/>
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<h1>main</h1>}/>
+        <Route path='/games' element={<h1>games</h1>}/>
+        <Route path='/about' element={<h1>about</h1>}/>
+      </Routes>
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../assets/css/navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='container mt-5'>
+        <a className=''>
+            Legendary Games
+        </a>
+    </div>
+  )
+}
