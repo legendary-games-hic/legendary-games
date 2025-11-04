29e974225f5f384224a71904b11295b6632de2aa
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-11-04 10:51:40 -0500

    fetching data example

--- README.md
+++ README.md
@@ -11,7 +11,19 @@
 - Background gray: 212121
 - Navbar/Footer gray: 121212
 
-
 ### How to run
 - Have node.JS installed
 - run `npm run dev` to start the webserver locally
+
+# API ey setup
+- For game data, we are using: https://api.rawg.io/docs/
+- You need to make an account and get an API key 
+- DO NOT put the API key in the code
+    - create a file called `.env.local` in the base project directory (on your local copy)
+    - The file contents must be just: `VITE_API_KEY=YOUR API KEY FROM RAWG`
+- To use the API key in your code:
+    - Add the following line on top of your `.jsx` file where you'll use the API key:
+```const APIKEY = import.meta.env.VITE_API_KEY```
+    - This will automatically import the key from the `.env.local` file and put it in that variable
+
+### API Requests Guide
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import axios from 'axios'
 
+const APIKEY = import.meta.env.VITE_API_KEY
+
 export default function MainPage() {
-    async function getData(){
-        const resp = await axios.get('https://api.rawg.io/api/games?dates=2019-09-01,2019-09-30&platforms=18,1,7', {
-        params: {
-            key: '5b5b60451aa943a196c703c3eef1b92c',
-            search: 'The Witcher 3'
+    const [data, setData] = useState('loading...')
+
+    useEffect(()=>{
+        let cancelled = false;
+        (async () => {
+        try {
+            const { data } = await axios.get(`https://api.rawg.io/api/games/64`,
+                {params: {key: `${APIKEY}`}}
+            );
+            if (!cancelled) {
+                setData(data);
+            }
+        } catch (e) {
+            if (!cancelled) setErr(e);
         }
-    });
+        })();
+        return () => { cancelled = true; };
+    }, [])
 
-    console.log(resp)
-    }
-
-    getData();
-
-  return (
-    <button className='btn-success'>MaindqdwqPage</button>
-  )
+    return (
+        <>
+            <button className='btn-success'>This is the main page, here is the image and raw JSON data for game with id <b>64</b>, name fetched from rawg api: <b>{data['name']}</b></button>
+            <img src={data['background_image']}>
+            </img>
+            <p>{JSON.stringify(data)}</p>
+        </>
+    )
 }
