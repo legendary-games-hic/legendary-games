a8cc60679bc0815e55cc52c39e6684e26b7d02d8
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-11-09 15:00:20 -0500

    font, colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 import '../assets/css/navbar.css'
 
 export default function Navbar() {
   return (
-    <div className='container mt-5'>
-        <a className=''>
+    <nav className='navbar bg-navbar-and-footer'>
+        <Link className='navbar-brand legendary-green-color' to='/'>
             Legendary Games
-        </a>
-    </div>
+        </Link>
+        <Link className='nav-item legendary-green-color' to='/'>
+            Library
+        </Link>
+        <Link className='nav-item legendary-green-color' to='/'>
+            Community
+        </Link>
+        <Link className='nav-item legendary-green-color' to='/'>
+            News
+        </Link>
+    </nav>
   )
 }
