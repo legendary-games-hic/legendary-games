26f3c2bae41b92c2b4ee33a753ab7279d877c8b4
Joao Donaton <joao.vitor.donaton@gmail.com>
2025-12-01 20:33:21 -0500

    support page link in 440

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -11,7 +11,7 @@ export default function NotFoundPage() {
         <i class="bi bi-emoji-frown legendary-green-color"></i>
         <h1>404 - Page Not Found</h1>
         <p>The page you are looking for ("{location.pathname}") does not exist. If you believe this is an error, please contact
-            Legendary Games staff through the support page. Click <Link to='/'>here</Link> to return home.
+            Legendary Games staff through the <Link to='/support'>support page</Link>. Click <Link to='/'>here</Link> to return home.
         </p>
     </div>
   )
